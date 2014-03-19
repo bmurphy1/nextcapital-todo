4ad0f30cce00ef35336138b71ce8c0f50786c7cf
Robert Clark <clarkr@businesslogic.com>
2014-03-19 17:37:20 -0500

    Improve sessions API.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,17 +8,17 @@ class SessionsController < Devise::SessionsController
   end
 
   def create
-    resource = User.find_for_database_authentication(:email=>params[:user][:email])
+    resource = User.find_for_database_authentication(:email => params[:email])
 
     return login_error "Couldn't find a user with that email." unless resource
 
-    if resource.valid_password?(params[:user][:password])
+    if resource.valid_password?(params[:password])
       resource.api_token = Devise.friendly_token
       resource.save!
 
       sign_in("user", resource)
 
-      render :json => resource and return
+      render :json => resource.to_json(:except => [:created_at, :updated_at]) and return
     end
 
     login_error "Password is not valid."
